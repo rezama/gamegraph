8fe7fd87b66f6d5e252b76e21b7661e430c8e599
reza <reza@tp>
2012-09-23 05:34:03 -0600

    Added stdout and stderr options to the generated condor scripts.

--- gamegraph/src/experiment_planner.py
+++ gamegraph/src/experiment_planner.py
@@ -4,6 +4,16 @@ Created on Sep 21, 2012
 @author: reza
 '''
 from common import FILE_CONDOR_PLAN
+from params import NUM_TRIALS
+
+def queue_job(f, args):
+    args_no_space = args.replace('app_', '').replace('.py', '').\
+                replace(' ', '-').replace('--', '-').replace('--', '-')
+    f.write('arguments = %s\n' % args) 
+    f.write('stdout = ../log/%s\n' % args_no_space)
+    f.write('stderr = ../log/%s\n' % args_no_space)
+    f.write('queue\n')
+    
 
 if __name__ == '__main__':
 
@@ -12,8 +22,8 @@ if __name__ == '__main__':
     f.write('+Project = "AI_ROBOTICS"\n')
     f.write('+ProjectDescription = "Studying impact of domain ergodicity on reinforcement learning and self-play"\n')
     f.write('\n')
-    f.write('Universe = vanilla\n')
-    f.write('Executable = /usr/bin/python\n')
+    f.write('universe = vanilla\n')
+    f.write('executable = /usr/bin/python\n')
     f.write('getenv = true\n')
     f.write('transfer_executable = false\n')  
     f.write('\n')
@@ -21,10 +31,12 @@ if __name__ == '__main__':
     for app in ['app_sarsa', 'app_ntd', 'app_hc']:
         for domain in ['minigammon', 'nannon', 'midgammon', 'nohitgammon']:
             for offset in [0, 1, 2, 3]:
-                for trial in range(10):
-                    f.write('Arguments = %s.py --domain %s --offset %d --trial %d\nQueue\n' % (app, domain, offset, trial))
+                for trial in range(NUM_TRIALS):
+                    args = '%s.py --domain %s --offset %d --trial %d' % (app, domain, offset, trial)
+                    queue_job(f, args)
             for chooseroll in ['0.0', '0.1', '0.5', '0.9', '1.0']:
-                for trial in range(10):
-                    f.write('Arguments = %s.py --domain %s --chooseroll %s --trial %d\nQueue\n' % (app, domain, chooseroll, trial))
+                for trial in range(NUM_TRIALS):
+                    args = '%s.py --domain %s --chooseroll %s --trial %d' % (app, domain, chooseroll, trial)
+                    queue_job(f, args)
 
     f.close()
